b2188d87eeb7301bb74d3d3b5125fbcded0c3d75
Socolin <Socolin@minefield.fr>
2016-12-17 14:21:45 -0500

    Improve js/css/fonts bundles

--- update.sh
+++ update.sh
@@ -3,11 +3,6 @@ git pull
 set -e
 ./node_modules/.bin/ng build -prod
 cd dist
-ln -s assets/css/
-ln -s assets/js/
-ln -s assets/fonts/
 ln -s assets/img/
 ln -s assets/favicon
-ln -s assets/favicon.ico 
-
-
+ln -s assets/favicon.ico
