a2dfec144ab0a5de115c5ce9ee9097f108fea101
Socolin <Socolin@minefield.fr>
2017-06-25 17:08:08 -0400

    Update polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -17,3 +17,8 @@ import 'core-js/es6/reflect';
 
 import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
+
+import 'web-animations-js/web-animations.min';
+import 'intl';
+import 'intl/locale-data/jsonp/en';
+import 'intl/locale-data/jsonp/fr';
