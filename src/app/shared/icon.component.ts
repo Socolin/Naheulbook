7980395b332d01df6fb312fac8a43b4f471c4b43
Socolin <Socolin@minefield.fr>
2017-07-22 16:17:06 -0400

    Add enchanted effect on icon for enchanted items

--- src/app/shared/icon.component.ts
+++ src/app/shared/icon.component.ts
@@ -4,40 +4,12 @@ import {IconDescription} from './icon.model';
 @Component({
     selector: 'icon',
     templateUrl: './icon.component.html',
-    styles: [
-        `.rotat0:before {
-            display: inline-block;
-        }
-        .rotate90:before {
-            display: inline-block;
-            transform: rotate(90deg) ;
-            -webkit-transform: rotate(90deg) ;
-            -moz-transform: rotate(90deg) ;
-            -o-transform: rotate(90deg) ;
-            -ms-transform: rotate(90deg) ;
-        }
-        .rotate180:before {
-            display: inline-block;
-            transform: rotate(180deg) ;
-            -webkit-transform: rotate(180deg) ;
-            -moz-transform: rotate(180deg) ;
-            -o-transform: rotate(180deg) ;
-            -ms-transform: rotate(180deg) ;
-        }
-        .rotate270:before {
-            display: inline-block;
-            transform: rotate(270deg) ;
-            -webkit-transform: rotate(270deg) ;
-            -moz-transform: rotate(270deg) ;
-            -o-transform: rotate(270deg) ;
-            -ms-transform: rotate(270deg) ;
-        }
-        `,
-    ]
+    styleUrls: ['./icon.component.scss']
 })
 export class IconComponent implements OnChanges {
     @Input() icon: IconDescription;
     @Input() size: string;
+    @Input() enchanted: boolean;
 
     public defaultIcon: IconDescription = {
         name: 'uncertainty',
