a236f402ae906676556de1c3b2af6509fe62651e
Socolin <Socolin@minefield.fr>
2017-12-03 15:14:55 -0500

    Simplify update script

--- update.sh
+++ update.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 git pull
 set -e
-./node_modules/.bin/ng build -prod --aot
-cd dist
-ln -s assets/img/
-ln -s assets/favicon
-ln -s assets/favicon.ico
+npm update
+./node_modules/.bin/ng build --prod
