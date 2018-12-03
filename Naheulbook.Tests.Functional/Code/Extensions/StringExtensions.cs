a936e8148cde26a3932fe6f431db45a5e3e22498
Socolin <provost.bertrand@gmail.com>
2018-12-02 22:39:40 -0500

    test: Throw when trying to replace elements in string from scenario context and the key is not present in it

--- Naheulbook.Tests.Functional/Code/Extensions/StringExtensions.cs
+++ Naheulbook.Tests.Functional/Code/Extensions/StringExtensions.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Text.RegularExpressions;
 using TechTalk.SpecFlow;
 
 namespace Naheulbook.Tests.Functional.Code.Extensions
@@ -7,9 +9,11 @@ namespace Naheulbook.Tests.Functional.Code.Extensions
         public static string ExecuteReplacement(this string str, ScenarioContext context)
         {
             foreach (var contextKey in context.Keys)
-            {
                 str = str.Replace($"${{{contextKey}}}", context[contextKey].ToString());
-            }
+
+            var match = Regex.Match(str, "\\${(?<Key>[a-z0-9A-Z_-]+)}");
+            if (match.Success)
+                throw new Exception($"Failed to key {match.Groups["Key"]} in scenario context when replacing '{str}'");
 
             return str;
         }
