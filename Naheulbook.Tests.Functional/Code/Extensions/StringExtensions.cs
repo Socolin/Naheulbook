2f441bcbf19b5a6678a7515670097c0cb0d8196e
Socolin <provost.bertrand@gmail.com>
2019-01-12 21:52:15 -0500

    test: Allow to use ${SomeKey.SomeProperty} to do some replacement from complex object stored in ScenarioContext

--- Naheulbook.Tests.Functional/Code/Extensions/StringExtensions.cs
+++ Naheulbook.Tests.Functional/Code/Extensions/StringExtensions.cs
@@ -1,5 +1,7 @@
 using System;
-using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using TechTalk.SpecFlow;
 
 namespace Naheulbook.Tests.Functional.Code.Extensions
@@ -8,14 +10,76 @@ namespace Naheulbook.Tests.Functional.Code.Extensions
     {
         public static string ExecuteReplacement(this string str, ScenarioContext context)
         {
-            foreach (var contextKey in context.Keys)
-                str = str.Replace($"${{{contextKey}}}", context[contextKey].ToString());
+            var replacementTokens = ListReplacementTokens(str);
 
-            var match = Regex.Match(str, "\\${(?<Key>[a-z0-9A-Z_-]+)}");
-            if (match.Success)
-                throw new Exception($"Failed to key {match.Groups["Key"]} in scenario context when replacing '{str}'");
+            foreach (var replacementToken in replacementTokens)
+            {
+                string replacementValue;
+                if (replacementToken.Contains("."))
+                {
+                    var replacements = replacementToken.Split('.');
+                    if (!context.Keys.Contains(replacements.First()))
+                        throw new Exception($"Failed to find key {replacements.First()} in scenario context when replacing '{str}'");
+                    var rootValue = context[replacements.First()];
+                    replacementValue = GetProperty(rootValue, replacements.Skip(1).ToList());
+                }
+                else
+                {
+                    if (!context.Keys.Contains(replacementToken))
+                        throw new Exception($"Failed to find key {replacementToken} in scenario context when replacing '{str}'");
+                    replacementValue = context[replacementToken].ToString();
+                }
+
+                str = str.Replace($"${{{replacementToken}}}", replacementValue);
+            }
 
             return str;
+        }
+
+        private static string GetProperty(object rootValue, List<string> propertiesNames)
+        {
+            var obj = rootValue;
+            try
+            {
+                foreach (var propertyName in propertiesNames)
+                {
+                    obj = obj.GetType().GetProperty(propertyName).GetValue(obj);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to find property `{string.Join('.', propertiesNames.Skip(1))}` in object.", ex);
+            }
+
+            return obj.ToString();
+        }
+
+        private static IEnumerable<string> ListReplacementTokens(string str)
+        {
+            var currentToken = new StringBuilder();
+            var inReplacement = false;
+            var previousChar = '\0';
+            var tokens = new List<string>();
+
+            foreach (var c in str)
+            {
+                if (previousChar == '$' && c == '{')
+                {
+                    inReplacement = true;
+                    currentToken.Clear();
+                }
+                else if (inReplacement && c == '}')
+                {
+                    tokens.Add(currentToken.ToString());
+                    inReplacement = false;
+                }
+                else if (inReplacement)
+                    currentToken.Append(c);
+
+                previousChar = c;
+            }
+
+            return tokens;
         }
     }
 }
