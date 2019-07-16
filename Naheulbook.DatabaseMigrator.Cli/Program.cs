5acc5c5980ab6ce76357bc5d153524a30ed6d8ab
Socolin <provost.bertrand@gmail.com>
2019-07-16 00:16:55 -0400

    ci: Databade migrator as an executable

---
+++ Naheulbook.DatabaseMigrator.Cli/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+using Naheulbook.DatabaseMigrator.Migrations;
+
+namespace Naheulbook.DatabaseMigrator.Cli
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var serviceProvider = CreateServices(args[0]);
+
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+
+        private static IServiceProvider CreateServices(string connectionString)
+        {
+            return new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    .AddMySql5()
+                    .WithGlobalConnectionString(connectionString)
+                    .ScanIn(typeof(Mig0001Init).Assembly).For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider(false);
+        }
+
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            runner.MigrateUp();
+        }
+    }
+}
