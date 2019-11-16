422dbd96436af7a49fa4ba11798c17af3bfb91c2
Socolin <provost.bertrand@gmail.com>
2019-11-16 17:39:46 -0500

    Create user for each dev database, since there is a bug in FluentMigrator
    https://github.com/fluentmigrator/fluentmigrator/issues/1114

--- Naheulbook.DatabaseMigrator/README.md
+++ Naheulbook.DatabaseMigrator/README.md
@@ -10,6 +10,12 @@ CREATE USER 'naheulbook'@'localhost' IDENTIFIED BY 'naheulbook';
 GRANT  ALL PRIVILEGES ON `naheulbook`.* TO 'naheulbook'@'localhost';
 GRANT  ALL PRIVILEGES ON `naheulbook_test`.* TO 'naheulbook'@'localhost';
 GRANT  ALL PRIVILEGES ON `naheulbook_integration`.* TO 'naheulbook'@'localhost';
+CREATE USER 'naheulbook_dev'@'localhost' IDENTIFIED BY 'naheulbook';
+GRANT  ALL PRIVILEGES ON `naheulbook`.* TO 'naheulbook_dev'@'localhost';
+CREATE USER 'naheulbook_test'@'localhost' IDENTIFIED BY 'naheulbook';
+GRANT  ALL PRIVILEGES ON `naheulbook_test`.* TO 'naheulbook_test'@'localhost';
+CREATE USER 'naheulbook_integration'@'localhost' IDENTIFIED BY 'naheulbook';
+GRANT  ALL PRIVILEGES ON `naheulbook_integration`.* TO 'naheulbook_integration'@'localhost';
 FLUSH PRIVILEGES;
 ```
 
--- Naheulbook.Tests.Functional/Code/Constants/DefaultTestConfigurations.cs
+++ Naheulbook.Tests.Functional/Code/Constants/DefaultTestConfigurations.cs
@@ -3,6 +3,14 @@ namespace Naheulbook.Tests.Functional.Code.Constants
     public static class DefaultTestConfigurations
     {
         public const string NaheulbookDbName = "naheulbook_test";
-        public static string NaheulbookTestConnectionString => $"Server=localhost;Database={NaheulbookDbName};Uid=naheulbook;Pwd=naheulbook;SslMode=None;CharSet=utf8;Allow User Variables=True";
+        public const string NaheulbookDbUserName = "naheulbook_test";
+
+        public static string NaheulbookTestConnectionString => "Server=localhost;" +
+                                                               $"Database={NaheulbookDbName};" +
+                                                               $"Uid={NaheulbookDbUserName};" +
+                                                               "Pwd=naheulbook;" +
+                                                               "SslMode=None;" +
+                                                               "CharSet=utf8;" +
+                                                               "Allow User Variables=True";
     }
 }
