5a74a826af69e4860bb9404a9af532feebe7c677
Socolin <provost.bertrand@gmail.com>
2019-11-17 22:20:34 -0500

    Add flag to display more details when an error occur

--- Naheulbook.Web/Middlewares/DevExceptionMiddleware.cs
+++ Naheulbook.Web/Middlewares/DevExceptionMiddleware.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Naheulbook.Core.Exceptions;
 using Naheulbook.Web.Exceptions;
 
 namespace Naheulbook.Web.Middlewares
@@ -10,11 +14,13 @@ namespace Naheulbook.Web.Middlewares
     {
         private readonly RequestDelegate _next;
         private readonly ILogger _logger;
+        private readonly bool _displayExceptionFields;
 
-        public DevExceptionMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        public DevExceptionMiddleware(RequestDelegate next, ILoggerFactory loggerFactory, IConfiguration configuration)
         {
             _next = next;
             _logger = loggerFactory.CreateLogger(nameof(DevExceptionMiddleware));
+            _displayExceptionFields = configuration.GetValue<bool>("DisplayExceptionFields");
         }
 
         public async Task InvokeAsync(HttpContext context)
@@ -33,6 +39,15 @@ namespace Naheulbook.Web.Middlewares
                 context.Response.StatusCode = StatusCodes.Status500InternalServerError;
                 context.Response.ContentType = "text/plain";
                 await context.Response.WriteAsync(ex.ToString());
+
+                if (_displayExceptionFields)
+                {
+                    var excludedNames = new[] {"TargetSite", "StackTrace", "Message", "Data", "InnerException", "HelpLink", "Source", "HResult"};
+                    foreach (var propertyInfo in ex.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public).Where(f => !excludedNames.Contains(f.Name)))
+                    {
+                        await context.Response.WriteAsync("\n" + propertyInfo.Name + "=" + propertyInfo.GetValue(ex));
+                    }
+                }
             }
         }
     }
