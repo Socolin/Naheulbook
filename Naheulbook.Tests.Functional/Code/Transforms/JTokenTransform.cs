9f2e546c99e80638bc185307eb7d25979e38ed02
Socolin <provost.bertrand@gmail.com>
2019-11-22 22:06:25 -0500

    Improve error message when there is a typo in a step definition

--- Naheulbook.Tests.Functional/Code/Transforms/JTokenTransform.cs
+++ Naheulbook.Tests.Functional/Code/Transforms/JTokenTransform.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
 using Naheulbook.Tests.Functional.Code.Extensions;
 using Naheulbook.TestUtils;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using TechTalk.SpecFlow;
 
@@ -22,7 +27,41 @@ namespace Naheulbook.Tests.Functional.Code.Transforms
             [StepArgumentTransformation]
             public JToken ReplaceJToken(string input)
             {
-                return JToken.Parse(input.ExecuteReplacement(_scenarioContext, _testDataUtil));
+                var expandedJson = input.ExecuteReplacement(_scenarioContext, _testDataUtil);
+                try
+                {
+                    return JToken.Parse(expandedJson);
+                }
+                catch (JsonReaderException ex)
+                {
+                    var splitJson = expandedJson.Split(new[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+
+                    var showErrorOutput = new StringBuilder();
+                    var contextSize = 3;
+                    for (var i = -contextSize; i <= contextSize; i++)
+                    {
+                        if (ex.LineNumber + i < 1)
+                            continue;
+                        if (ex.LineNumber + i >= splitJson.Length)
+                            continue;
+
+                        var line = splitJson[ex.LineNumber - 1 + i];
+                        showErrorOutput.Append((ex.LineNumber + i).ToString().PadRight(4, ' ')).Append('|');
+                        showErrorOutput.AppendLine(line);
+                        if (i == 0)
+                        {
+                            showErrorOutput.AppendLine("".PadLeft(ex.LinePosition + 5) + "^-- " + ex.Message);
+                        }
+                    }
+
+                    var stackTraceScenarioFrame = new StackTrace(true).GetFrames().FirstOrDefault(x => x.GetFileName()?.EndsWith(".feature") == true);
+
+                    throw new Exception($"During the scenario '{_scenarioContext.ScenarioInfo.Title}'\nwhile parsing step `{_scenarioContext.StepContext.StepInfo.Text}`" +
+                                        $"\nInvalid JSON found. At line {ex.LineNumber} at position: {ex.LinePosition}" +
+                                        $"\n\n{showErrorOutput}" +
+                                        $"\n\nIn file {stackTraceScenarioFrame?.GetFileName()}:{stackTraceScenarioFrame?.GetFileLineNumber()}\n\n"
+                        , ex);
+                }
             }
         }
     }
