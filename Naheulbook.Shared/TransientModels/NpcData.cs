3221971a535a0a61d909d38a56222ceda6f36854
Socolin <provost.bertrand@gmail.com>
2019-11-23 17:42:47 -0500

    Add new field in NpcData

--- Naheulbook.Shared/TransientModels/NpcData.cs
+++ Naheulbook.Shared/TransientModels/NpcData.cs
@@ -2,7 +2,9 @@ namespace Naheulbook.Shared.TransientModels
 {
     public class NpcData
     {
-        public string Location { get; set; }
-        public string Note { get; set; }
+        public string? Location { get; set; }
+        public string? Note { get; set; }
+        public string? OriginName { get; set; }
+        public string? Sex { get; set; }
     }
 }
