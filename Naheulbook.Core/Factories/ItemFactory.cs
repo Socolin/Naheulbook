1d226ca00d8f2d84af262b9cb6f2999e2b27ee49
Socolin <provost.bertrand@gmail.com>
2019-11-27 19:40:02 -0500

    When creating an item, use given icon instead of item template icon

--- Naheulbook.Core/Factories/ItemFactory.cs
+++ Naheulbook.Core/Factories/ItemFactory.cs
@@ -53,12 +53,11 @@ namespace Naheulbook.Core.Factories
 
             if (itemTemplateData.Charge.HasValue)
                 itemData.Charge = itemTemplateData.Charge.Value;
-            if (itemTemplateData.Icon != null)
-                itemData.Icon = itemTemplateData.Icon;
             if (itemTemplateData.Lifetime != null)
                 itemData.Lifetime = itemTemplateData.Lifetime;
             if (itemTemplateData.Quantifiable == true && itemData.Quantity == null)
                 itemData.Quantity = 1;
+            itemData.Icon ??= itemTemplateData.Icon;
             if (string.IsNullOrEmpty(itemData.Name))
             {
                 if (itemData.NotIdentified == true && !string.IsNullOrEmpty(itemTemplateData.NotIdentifiedName))
