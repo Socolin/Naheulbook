74f030095764a6b1b98164646502f6dc80a24e18
Socolin <provost.bertrand@gmail.com>
2021-05-04 00:15:50 -0400

    Update game-icons

--- libs/game-icons/update-games-icon.sh
+++
@@ -1,5 +0,0 @@
-echo 'TRUNCATE icon;'
-echo 'INSERT INTO icon VALUES '
-cat game-icons.css  | grep :before | sed -r 's/^.game-icon-//g' |sed -r 's/:before \{//'  | sort |  sed -r "s/(.+)/('\1'),/"
-echo ';'
-
--- libs/game-icons/update-icons-sql.sh
+++
@@ -1,2 +0,0 @@
-echo 'INSERT IGNORE INTO icon(name) VALUES' && cat game-icons.css  | grep :before | sed -r 's/^.game-icon-//g' |sed -r 's/:before \{//'  | sort |  sed -r "s/(.+)/('\1'),/" | sed '$s/\,/;/g'
-
