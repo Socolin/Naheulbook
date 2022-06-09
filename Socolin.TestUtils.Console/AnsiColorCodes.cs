01d227fb4a19b13a0ccfe3ffa584b2da1b26cc07
Socolin <provost.bertrand@gmail.com>
2022-06-09 01:52:47 -0400

    Support color even when console is redirected for dotnet test (seems NUnit is blocking escape character now...)

--- Socolin.TestUtils.Console/AnsiColorCodes.cs
+++ Socolin.TestUtils.Console/AnsiColorCodes.cs
@@ -2,30 +2,38 @@ namespace Socolin.TestUtils.Console;
 
 public class AnsiColorCodes
 {
-    public const string Reset = $"{EscapeCode}[0m";
+    public static bool ForceColor = Environment.GetEnvironmentVariable("FORCE_COLOR") == "true";
+    private const string ResetCode = $"{EscapeCode}[0m";
     private const string EscapeCode = "\u001b";
 
     public static string Color(Terminal8ColorCodes code)
     {
-        if (System.Console.IsOutputRedirected)
+        if (!ForceColor && System.Console.IsOutputRedirected)
             return string.Empty;
         return $"{EscapeCode}[" + (int)code + "m";
     }
 
+    public static string Reset()
+    {
+        if (!ForceColor && System.Console.IsOutputRedirected)
+            return string.Empty;
+        return ResetCode;
+    }
+
     public static string Color(string text, Terminal8ColorCodes code)
     {
-        return Color(code) + text + Reset;
+        return Color(code) + text + Reset();
     }
 
     public static string Color256(Terminal256ColorCodes code)
     {
-        if (System.Console.IsOutputRedirected)
+        if (!ForceColor && System.Console.IsOutputRedirected)
             return string.Empty;
         return $"{EscapeCode}[38;5;{(int)code}m";
     }
 
     public static string Color256(string text, Terminal256ColorCodes code)
     {
-        return Color256(code) + text + Reset;
+        return Color256(code) + text + Reset();
     }
 }
