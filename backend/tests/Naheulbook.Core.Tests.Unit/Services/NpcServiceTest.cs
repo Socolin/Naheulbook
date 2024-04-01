5bf57aeb1ae2cb30da294aa9f13894fabb6353c0
Socolin <provost.bertrand@gmail.com>
2024-04-01 14:44:14 -0400

    Upgrade npm packages

---
+++ backend/tests/Naheulbook.Core.Tests.Unit/Services/NpcServiceTest.cs
@@ -0,0 +1,11 @@
+using Naheulbook.Core.Services;
+using NUnit.Framework;
+
+namespace Naheulbook.Core.Tests.Unit.Services;
+
+[TestFixture]
+[TestOf(typeof(NpcService))]
+public class NpcServiceTest
+{
+
+}
